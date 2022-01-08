f56519092b51309cecad07d7856fe6039fd70162
Kasekopf <jeffreydudek@gmail.com>
2022-01-07 21:32:34 -0800

    Add additional debugging printouts to main loop

--- src/lib.ts
+++ src/lib.ts
@@ -1,5 +1,9 @@
 import { print } from "kolmafia";
 
-export function debug(message: string): void {
-  print(message);
+export function debug(message: string, color?: string): void {
+  if (color) {
+    print(message, color);
+  } else {
+    print(message);
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,27 @@ import { myAdventures } from "kolmafia";
 import { all_tasks } from "./tasks/all";
 import { prioritize } from "./route";
 import { Engine } from "./engine";
+import { debug } from "./lib";
 
 export function main(): void {
   const tasks = prioritize(all_tasks());
   const engine = new Engine(tasks);
   while (myAdventures() > 0) {
     const todo = tasks.find((task) => engine.available(task));
-    if (todo === undefined) throw `Unable to find task`;
+    if (todo === undefined) {
+      const remaining_tasks = tasks.filter((task) => !task.completed());
+      if (remaining_tasks.length === 0) break;
+
+      for (const task of remaining_tasks) {
+        if (!task.completed()) debug(`${task.name}`, "red");
+      }
+      throw `Unable to find available task, but not all tasks are completed.`;
+    }
+
+    debug("");
+    debug(`Executing ${todo.name}`, "blue");
     engine.execute(todo);
+    if (todo.completed()) debug(`${todo.name} completed!`, "blue");
+    else debug(`${todo.name} not completed!`, "blue");
   }
 }
