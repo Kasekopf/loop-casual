950e751c3eec4b27a213c228e39ad30a2402efdf
Kasekopf <jeffreydudek@gmail.com>
2022-01-08 01:07:08 -0800

    Change script name

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   entry: {
     // Define files webpack will emit, does not need to correspond 1:1 with every typescript file
     // You need an emitted file for each entrypoint into your code, e.g. the main script and the ccs or ccs consult script it calls
-    "main-script-name": "./src/main.ts",
+    mycasual: "./src/main.ts",
   },
   // Turns on tree-shaking and minification in the default Terser minifier
   // https://webpack.js.org/plugins/terser-webpack-plugin/
