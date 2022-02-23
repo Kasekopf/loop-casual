83480c3f488e0f4176328241109f56e84a1910ef
Kasekopf <jeffreydudek@gmail.com>
2022-02-22 23:45:33 -0800

    Tune airship noncombat towards combat

--- src/tasks/level10.ts
+++ src/tasks/level10.ts
@@ -29,7 +29,7 @@ export const GiantQuest: Quest = {
       after: ["Grow Beanstalk"],
       completed: () => have($item`S.O.C.K.`),
       do: $location`The Penultimate Fantasy Airship`,
-      choices: { 178: 2, 182: 2 },
+      choices: { 178: 2, 182: 1 },
       post: () => {
         if (have($effect`Temporary Amnesia`)) cliExecute("uneffect Temporary Amnesia");
       },
