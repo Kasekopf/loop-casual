47db37ae88d77f3250ea6cca93503194c4212f62
Kasekopf <jeffreydudek@gmail.com>
2022-06-06 21:01:08 -0700

    Add some default values for the June cleaver to avoid crashing

--- src/main.ts
+++ src/main.ts
@@ -266,5 +266,15 @@ function setUniversalProperties(propertyManager: PropertiesManager) {
     1107: 1, // tennis ball
     1340: 3, // Is There A Doctor In The House?
     1341: 1, // Cure her poison
+    // June cleaver noncombats
+    1467: 1,
+    1468: 1,
+    1469: 2,
+    1470: 2,
+    1471: 1,
+    1472: 2,
+    1473: 2,
+    1474: 2,
+    1475: 1,
   });
 }
