b6d1ebc7293a0a40dbc98a9f80f630957170f0a6
Kasekopf <jeffreydudek@gmail.com>
2022-09-23 20:02:24 -0700

    Handle parka folding

--- src/engine/outfit.ts
+++ src/engine/outfit.ts
@@ -208,4 +208,20 @@ export function fixFoldables(outfit: Outfit) {
       cliExecute("retrocape vampire hold");
     }
   }
+
+  // Libram outfit cache may not autofold parka, so we need to
+  if (equippedAmount($item`Jurassic Parka`) > 0) {
+    if (outfit.modifier?.includes("cold res")) {
+      if (get("parkaMode").toLowerCase() !== "kachungasaur") cliExecute("parka kachungasaur");
+    } else if (outfit.modifier?.includes("stench res")) {
+      if (get("parkaMode").toLowerCase() !== "dilophosaur") cliExecute("parka dilophosaur");
+    } else if (outfit.modifier?.includes("ML") && !outfit.modifier.match("-[\\d .]*ML")) {
+      if (get("parkaMode").toLowerCase() !== "spikolodon") cliExecute("parka spikolodon");
+    } else if (outfit.modifier?.includes("init") || outfit.modifier?.includes("-combat")) {
+      if (get("parkaMode").toLowerCase() !== "pterodactyl") cliExecute("parka pterodactyl");
+    } else {
+      // +meat
+      if (get("parkaMode").toLowerCase() !== "kachungasaur") cliExecute("parka kachungasaur");
+    }
+  }
 }
