9e8c3f5755726b40e6ae56c669246851d50bc7f1
Kasekopf <jeffreydudek@gmail.com>
2022-09-26 18:05:29 -0700

    Pull meat a bit sooner

--- src/tasks/pulls.ts
+++ src/tasks/pulls.ts
@@ -63,7 +63,7 @@ export const pulls: PullSpec[] = [
   {
     pull: $item`1,970 carat gold`,
     useful: () => {
-      if (myMeat() < 200 && step("questM05Toot") > 0 && !have($item`letter from King Ralph XI`))
+      if (myMeat() < 250 && step("questM05Toot") > 0 && !have($item`letter from King Ralph XI`))
         return true;
       if (
         myMeat() < 4000 &&
