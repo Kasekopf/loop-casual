3692fb26de164d73533541d470e1b61c30338b5d
Kasekopf <jeffreydudek@gmail.com>
2022-11-15 19:00:35 -0800

    Prioritize delay zones with all absorbs done

--- src/engine/engine.ts
+++ src/engine/engine.ts
@@ -176,15 +176,36 @@ export class Engine extends BaseEngine<CombatActions, ActiveTask> {
 
     // If a wanderer is up try to place it in a useful location
     const wanderer = wandererSources.find((source) => source.available() && source.chance() === 1);
-    const delay_burning = available_tasks.find(
-      (task) => this.hasDelay(task) && this.createOutfit(task).canEquip(wanderer?.equip ?? [])
-    );
-    if (wanderer !== undefined && delay_burning !== undefined) {
-      return {
-        ...delay_burning,
-        active_priority: Prioritization.fixed(OverridePriority.Wanderer),
-        wanderer: wanderer,
-      };
+    if (wanderer) {
+      const absorb_state = globalStateCache.absorb();
+      // Prefer delay burning where there are no more absorbs to find
+      const delay_burning_no_absorbs = available_tasks.find(
+        (task) =>
+          this.hasDelay(task) &&
+          this.createOutfit(task).canEquip(wanderer?.equip ?? []) &&
+          !(
+            task.do instanceof Location &&
+            (absorb_state.hasTargets(task.do) || absorb_state.hasReprocessTargets(task.do))
+          )
+      );
+      if (delay_burning_no_absorbs) {
+        return {
+          ...delay_burning_no_absorbs,
+          active_priority: Prioritization.fixed(OverridePriority.Wanderer),
+          wanderer: wanderer,
+        };
+      }
+
+      const delay_burning = available_tasks.find(
+        (task) => this.hasDelay(task) && this.createOutfit(task).canEquip(wanderer?.equip ?? [])
+      );
+      if (delay_burning) {
+        return {
+          ...delay_burning,
+          active_priority: Prioritization.fixed(OverridePriority.Wanderer),
+          wanderer: wanderer,
+        };
+      }
     }
 
     // Next, choose tasks by priorty, then by route.
