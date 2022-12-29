45817ae74bfc25383d00cd92fe8ad66ec3563669
Kasekopf <jeffreydudek@gmail.com>
2022-12-28 22:21:27 -0800

    Increase KolMafia

--- src/main.ts
+++ src/main.ts
@@ -29,7 +29,7 @@ const time_property = "_loop_gyou_first_start";
 const svn_name = "Kasekopf-loop-casual-branches-release";
 
 export function main(command?: string): void {
-  sinceKolmafiaRevision(26834);
+  sinceKolmafiaRevision(27020);
 
   Args.fill(args, command);
   if (args.debug.settings) {
