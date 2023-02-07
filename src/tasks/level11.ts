9dc18b056e099be34ec3ae0ab9f5d66588135f3a
Kasekopf <jeffreydudek@gmail.com>
2023-02-06 22:55:06 -0800

    Handle autumnaton returning during ed

--- src/tasks/level11.ts
+++ src/tasks/level11.ts
@@ -59,7 +59,7 @@ const Diary: Task[] = [
         globalStateCache.absorb().isReprocessTarget($monster`black magic woman`) &&
         familiarWeight($familiar`Grey Goose`) >= 6 &&
         globalStateCache.orb().prediction($location`The Black Forest`) ===
-          $monster`black magic woman`
+        $monster`black magic woman`
       ) {
         // Swoop in for a single adventure to reprocess the black magic woman
         return {
@@ -370,6 +370,10 @@ const Pyramid: Task[] = [
     after: ["Use Bomb"],
     completed: () => step("questL11Pyramid") === 999,
     do: () => visitUrl("place.php?whichplace=pyramid&action=pyramid_state1a"),
+    post: () => {
+      // Autunmaton returning is not properly tracked
+      cliExecute("refresh all");
+    },
     outfit: () => {
       if (!have($item`Pick-O-Matic lockpicks`) && !towerSkip())
         return { familiar: $familiar`Gelatinous Cubeling` }; // Ensure we get equipment
