e7d3df08f1130d2ad6dae9f85915eb0bf0a33065
libraryaddict <github@lib.co.nz>
2023-02-11 05:06:03 +1300

    Better rock garden harvesting

--- src/tasks/misc.ts
+++ src/tasks/misc.ts
@@ -6,6 +6,7 @@ import {
   equippedAmount,
   Familiar,
   familiarWeight,
+  getCampground,
   getWorkshed,
   gnomadsAvailable,
   haveEquipped,
@@ -925,11 +926,12 @@ export const MiscQuest: Quest = {
       after: [],
       priority: () => Priorities.Free,
       ready: () => haveInCampground($item`packet of rock seeds`),
-      completed: () => get("desertExploration") > 0 || have($item`milestone`),
+      completed: () => getCampground()[$item`milestone`.name] === 0,
       do: () => {
         visitUrl("campground.php?action=rgarden1&pwd");
         visitUrl("campground.php?action=rgarden2&pwd");
         visitUrl("campground.php?action=rgarden3&pwd");
+        visitUrl("campground.php");
       },
       limit: { tries: 1 },
       freeaction: true,
