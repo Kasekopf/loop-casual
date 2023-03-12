d697cee7386d4f2df7360b881c5fbf8f803c2c9c
libraryaddict <github@lib.co.nz>
2023-03-13 06:15:59 +1300

    Update voter booth for slime/snake prioritizing inline with garbo

--- src/tasks/misc.ts
+++ src/tasks/misc.ts
@@ -6,6 +6,7 @@ import {
   equippedAmount,
   Familiar,
   familiarWeight,
+  gamedayToInt,
   getCampground,
   getWorkshed,
   gnomadsAvailable,
@@ -201,11 +202,11 @@ export const MiscQuest: Quest = {
           },
           {
             monster: $monster`annoyed snake`,
-            value: 25 * 0.5 + 25,
+            value: gamedayToInt(),
           },
           {
             monster: $monster`slime blob`,
-            value: 20 * 0.4 + 50 * 0.2 + 250 * 0.01,
+            value: 95 - gamedayToInt(),
           },
         ];
 
