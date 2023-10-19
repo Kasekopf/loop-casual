bfd63c4afba4d974a47eae5fe78307f90a51532f
Ignose <ihpares@gmail.com>
2023-10-19 12:34:36 -0400

    Prefer EasyFax as Cheesefax ded

--- src/tasks/summons.ts
+++ src/tasks/summons.ts
@@ -236,7 +236,7 @@ const summonSources: SummonSource[] = [
     canFight: (mon: Monster) => canFaxbot(mon),
     summon: (mon: Monster) => {
       // Default to CheeseFax unless EasyFax is the only faxbot online
-      const faxbot = ["CheeseFax", "EasyFax"].find(bot => isOnline(bot)) ?? "CheeseFax";
+      const faxbot = ["CheeseFax", "EasyFax"].find(bot => isOnline(bot)) ?? "EasyFax";
       for (let i = 0; i < 6; i++) {
         if (i % 3 === 0) chatPrivate(faxbot, mon.name);
         wait(10 + i);
